7a80b07bb6ad69466ad7eb471ff6c2325bf7329c
bynned <benny.cascarino@tuni.fi>
2024-01-30 13:48:32 +0200

    Default Spring project

---
+++ book-tracker-backend/src/main/java/com/booktracker/booktrackerbackend/BookTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.booktracker.booktrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ book-tracker-backend/src/test/java/com/booktracker/booktrackerbackend/BookTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booktracker.booktrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
