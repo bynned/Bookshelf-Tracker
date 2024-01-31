b39bb07ca532ca74d0d37e1dc214a976f21e3d98
bynned <benny.cascarino@tuni.fi>
2024-01-31 10:18:22 +0200

    Book entity class

---
+++ book-tracker-backend/src/main/java/com/booktracker/booktrackerbackend/Book.java
@@ -0,0 +1,38 @@
+package com.booktracker.booktrackerbackend;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor() {
+        this.author = author;
+    }
+}
